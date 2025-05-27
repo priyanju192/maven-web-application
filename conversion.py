3965f8ea776269d1fc5cbb08d98ab0374a86a65d
priyanju192 <160465322+priyanju192@users.noreply.github.com>
2025-05-27 12:32:00 -0400

    Update conversion.py

--- conversion.py
+++ conversion.py
@@ -1,18 +1,26 @@
-import json
 import csv
 
-with open('/var/jenkins_home/reports/trivy-report.json') as jf:
-    jd = json.load(jf)
+f = open('/var/jenkins_home/reports/trivy-report.json')
+data = json.load(f)
+f.close()
 
-df = open('/var/jenkins_home/reports/rep-jsonoutput.csv', 'w', newline='')
-cw = csv.writer(df)
+new_data = []
 
-c = 0
-for data in jd:
-    if c == 0:
-        header = data.keys()
-        cw.writerow(header)
-        c += 1
-    cw.writerow(data.values())
+for i in data:
+   flat = {}
+   names = i.keys()
+   for n in names:
+      try:
+         if len(i[n].keys()) > 0:
+            for ii in i[n].keys():
+               flat[n+"_"+ii] = i[n][ii]
+      except:
+         flat[n] = i[n]
+   new_data.append(flat)  
 
-df.close()
+f = open(filename, "r")
+writer = csv.DictWriter(f, new_data[0].keys())
+writer.writeheader()
+for row in new_data:
+   writer.writerow(row)
+f.close()
