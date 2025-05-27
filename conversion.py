3715778dadd3f27899e1209a74052a76c26929c9
priyanju192 <160465322+priyanju192@users.noreply.github.com>
2025-05-27 11:12:00 -0400

    Add files via upload

---
+++ conversion.py
@@ -0,0 +1,18 @@
+import json
+import csv
+
+with open('/var/jenkins_home/reports/trivy-report.json') as jf:
+    jd = json.load(jf)
+
+df = open('/var/jenkins_home/reports/jsonoutput.csv', 'w', newline='')
+cw = csv.writer(df)
+
+c = 0
+for data in jd:
+    if c == 0:
+        header = data.keys()
+        cw.writerow(header)
+        c += 1
+    cw.writerow(data.values())
+
+df.close()
